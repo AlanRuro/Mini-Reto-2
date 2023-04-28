40dcd5790460970cb6abe2a16f66a70b62b4baff
Alan Ruelas <A01641426@tec.mx>
2023-04-27 21:22:49 -0600

    Se agrega estructura base de la api

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -19,29 +19,68 @@ const connection = mysql.createConnection({
     port: 3306
   });
 
-connection.connect();
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to DB");
+});
 
-app.get("/api/empresa", async (req, res) => {
-    let sqlQuery = 'SELECT * FROM job';
-    connection.query(sqlQuery, (err, rows) => {
-        if (err) throw err;
+app.get("/api/cartoons", (req, res) => {
+    let sqlQuery = "SELECT * FROM cartoons;";
+    connection.query(sqlQuery, (error, rows) => {
+        if (error) throw error;
         res.send(rows);
     });
-})
+});
 
-app.get("/api/job/:id", async (req, res) => {
-    const userId = req.params.id;
-    const values = [userId];
-    let sql = 'SELECT * FROM job WHERE Job_Code = ?'
-    connection.query(sql, values, (err, row) => {
-        if (err) throw err;
+app.get("/api/cartoons/:cartoon_id", (req, res) => {
+    const cartoonId = req.params.cartoon_id;
+    let sqlQuery = "SELECT * FROM cartoons WHERE cartoon_id = ?;";
+    connection.query(sqlQuery, cartoonId, (error, row) => {
+        if (error) throw error;
         res.send(row);
     });
-})
+});
 
-app.get("/api/hello", (req, res) => {
-    res.json({message: "Hello form server side!"});
-})
+app.post("/api/cartoons", (req, res) => {
+    const { 
+        body: { cartoon_name, cartoon_channel }
+    } = req;
+    const values = [cartoon_name, cartoon_channel];
+    let sqlQuery = "INSERT INTO cartoons(cartoon_name, cartoon_channel) VALUES (?, ?);";
+    connection.query(sqlQuery, values, (error, rows) => {
+        if (error) throw error;
+        res.send(rows);
+    });
+});
+
+app.patch("/api/cartoons:cartoon_id)", (req, res) => {
+    const {
+        params: { cartoon_id },
+        body
+    } = req;
+    let values = [];
+    let sqlQuery = "UPDATE cartoons SET ";
+    for (const key in body) {
+        sqlQuery += `${key} = ?, `;
+        values.push(body[key]);
+    }
+    sqlQuery = sqlQuery.slice(0, -2);
+    sqlQuery += " WHERE cartoon_id = ?;";
+    values.push(cartoon_id);
+    connection.query(sqlQuery, values, (error, rows) => {
+        if (error) throw error;
+        res.sendStatus(201);
+    });
+});
+
+app.delete("/api/cartoons/:cartoon_id", (req, res) => {
+    const cartoonId = req.params.cartoon_id;
+    let sqlQuery = "DELETE FROM cartoons WHERE cartoon_id = ?;";
+    connection.query(sqlQuery, cartoonId, (error, result) => {
+        if (error) throw error;
+        res.sendStatus(204);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
